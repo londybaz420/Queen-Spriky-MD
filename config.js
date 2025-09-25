a3668b9292396a44ac5cfd66d0dd7ef5b6406d3c
londybaz420 <freedeployment101@gmail.com>
2025-09-25 10:10:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || '9edwQJwa#wR-WQfkxyRY0LfC2DnJkc3mUMRYcTa1DjqOcHwNATmM', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://shahzadabaloooch:DfdSqWiEsbDxefG9@cluster0.jae9sf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',// Enter Your MongoDB URL
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '923143200187',    // Enter Your Owner Number
 };
